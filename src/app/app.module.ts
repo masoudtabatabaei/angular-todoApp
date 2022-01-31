f4cabd51a438b8bfe679ce643b281faf18161f9b
Masoud Tabatabaei <masoudtabatabaei20@gmail.com>
2022-01-31 11:56:32 +0330

    generate todoForm component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TodoInputComponent } from './todo-input/todo-input.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TodoInputComponent
+    TodoInputComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule
