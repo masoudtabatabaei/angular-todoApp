6aa6e8e1b1f48ae1995f4d30f295b4424baf54b8
Masoud Tabatabaei <masoudtabatabaei20@gmail.com>
2022-01-31 13:31:46 +0330

    implement form button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TodoInputComponent } from './todo-input/todo-input.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoButtonComponent } from './todo-button/todo-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TodoInputComponent,
-    TodoFormComponent
+    TodoFormComponent,
+    TodoButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo-button/todo-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-button',
+  templateUrl: './todo-button.component.html',
+  styleUrls: ['./todo-button.component.css']
+})
+export class TodoButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
